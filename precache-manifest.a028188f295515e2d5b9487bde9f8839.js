acafca27f1b8847dd86a1df5c012883b78c66508
jwesinspace <westracompsci@gmail.com>
2019-02-17 08:02:36 -0500

    Committing WTT React site

---
+++ precache-manifest.a028188f295515e2d5b9487bde9f8839.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = [
+  {
+    "revision": "590e19508ab20b122da7153a712479ee",
+    "url": "/static/media/wt2.590e1950.jpg"
+  },
+  {
+    "revision": "2ffb34077ed59c85a72a",
+    "url": "/static/css/main.456a965a.chunk.css"
+  },
+  {
+    "revision": "978cee08eb2d5ce33565",
+    "url": "/static/js/1.978cee08.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "4c51c2d7370bd527f2cefc82860a2063",
+    "url": "/static/media/thai1.4c51c2d7.jpg"
+  },
+  {
+    "revision": "e4852d964cd4bcf4f4b1fd57a79129bd",
+    "url": "/static/media/thai2.e4852d96.jpg"
+  },
+  {
+    "revision": "63275c21c7c6d0de17c05f03071f6efa",
+    "url": "/static/media/th3.63275c21.jpg"
+  },
+  {
+    "revision": "c2bf3c502f34b12c89a1e76cea358fee",
+    "url": "/static/media/th4.c2bf3c50.jpg"
+  },
+  {
+    "revision": "a7bfef4ea834f7effdeaae54b8883e69",
+    "url": "/static/media/thai5.a7bfef4e.jpg"
+  },
+  {
+    "revision": "cf52e3e53402a551cb3342312ef01039",
+    "url": "/static/media/noodle1.cf52e3e5.jpg"
+  },
+  {
+    "revision": "a63f77d5179e7b9008d0fe8d37a45cdc",
+    "url": "/static/media/noodle2.a63f77d5.jpg"
+  },
+  {
+    "revision": "fd92670d4797756b507f2b1856380c01",
+    "url": "/static/media/wt1.fd92670d.jpg"
+  },
+  {
+    "revision": "2ffb34077ed59c85a72a",
+    "url": "/static/js/main.2ffb3407.chunk.js"
+  },
+  {
+    "revision": "c19bcb16d235c8ac898066fa4fff0c81",
+    "url": "/static/media/dessert1.c19bcb16.jpg"
+  },
+  {
+    "revision": "e3b0f56b395b57a4288bb5c3f39cb08e",
+    "url": "/static/media/wtt-storefront.e3b0f56b.png"
+  },
+  {
+    "revision": "887aee6b1c92310da3a9b89790e22a9e",
+    "url": "/static/media/noodle-crop-right.887aee6b.jpg"
+  },
+  {
+    "revision": "1a8bce63dc20830e997849f9c8d408ff",
+    "url": "/static/media/noodle2-crop-right.1a8bce63.jpg"
+  },
+  {
+    "revision": "5397416f2a547c4f81a16df776737976",
+    "url": "/static/media/dessert-crop-right.5397416f.jpg"
+  },
+  {
+    "revision": "d4baf523472650c8fa8536b7c5a85a30",
+    "url": "/static/media/noodle-crop-left.d4baf523.jpg"
+  },
+  {
+    "revision": "47dfceaafe3274c1e669e80ac36191d9",
+    "url": "/static/media/noodle2-crop-left.47dfceaa.jpg"
+  },
+  {
+    "revision": "2947166463e0c3fcf41839b320c138b4",
+    "url": "/static/media/dessert-crop-left.29471664.jpg"
+  },
+  {
+    "revision": "5c14743d00ecf84f82075a689a075446",
+    "url": "/static/media/tiger_words.5c14743d.png"
+  },
+  {
+    "revision": "5bd10b45d7b968aac7d0a7bb715414a6",
+    "url": "/static/media/large-restaurant.5bd10b45.jpg"
+  },
+  {
+    "revision": "d38d1ac387c068cefeb1db88d5dfda4c",
+    "url": "/index.html"
+  }
+];
